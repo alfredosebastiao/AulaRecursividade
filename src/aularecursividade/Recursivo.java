17357d8176b33d397a1d6efe853dfc6a12396c2a
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-09-04 15:01:10 -0700

    Nosso segundo commit

---
+++ src/aularecursividade/Recursivo.java
@@ -0,0 +1,17 @@
+
+package aularecursividade;
+
+/**
+ *
+ * @author Alfredo
+ * @author Turma
+ * @version 1.0
+ */
+public class Recursivo {
+    
+    
+    public boolean retorna(){
+        return true;
+    }
+    
+}
